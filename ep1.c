520de3fe3e52f25606ba56b54adc92fe947f785f
Gabriel Vieira <75680725+gabriel-vieira06@users.noreply.github.com>
2022-05-07 11:27:25 -0300

    Feat: portabilidade para sistemas IOS

--- ep1.c
+++ ep1.c
@@ -13,10 +13,21 @@
 
 #define EPSILON 0.00000001	//10^-8
 
+// Funcoes primarias
+
 void abreMenu();
 void conversaoNumerica();
 void recebeSistemaLinear();
 void lerEquacaoAlgebrica();
+
+// Funcoes auxiliares
+
+void converteDecimal(int base, char* valorConvertido, double decimal);
+double** criaMatriz (int l, int c);
+void metodoGaussSeidel(int numeroVariaveis, double* solucoes, double** m);
+void teoremaLagrange(double n, double an, double* coeficientes, double* limites, int contLimites);
+void metodoNewton(double* limites, int grauEquacao, double* coeficientes);
+void swap(double *a, double *b);
 
 void main()
 {
@@ -73,79 +84,12 @@ void conversaoNumerica()
 	double decimal;
 	char hexadecimal[100], octal[100], binario[100];
 	
-	void converteDecimal(int base, char* valorConvertido)
-	{
-		/*
-			Converte o valor decimal dado pelo usuario.
-			A base da conversao e a string onde a conversao sera armazenada
-			sao passados como parametros.
-			
-			ex: converteDecimal(2, binario), armazena na string binario a conversao
-			do valor decimal para um sistema numerico posicional de base 2
-		*/
-		
-		int parte_inteira = (int) decimal;
-		double parte_fracionaria = decimal - parte_inteira;
-		int posicao = 1, valor_temp, i;
-		char aux;
-		
-		if(decimal < 0)						//	Verifica o sinal do decimal e garante que a conversao
-		{									//	tera o mesmo sinal
-			valorConvertido[0] = '-';
-			parte_inteira *= -1;
-		} else valorConvertido[0] = ' ';
-		
-		if(parte_inteira == 0)				// Caso nao haja parte inteira, a conversao tambem nao tera
-		{
-			valorConvertido[posicao] = '0';
-			posicao++;
-		}
-		
-		if(parte_fracionaria < 0) parte_fracionaria *= -1;
-		
-		while(parte_inteira != 0)					//	Convertendo a parte inteira, realiza-se divisoes inteiras
-		{											//	e armazenamos o valor do resto
-			valor_temp = parte_inteira % base;
-			if(valor_temp < 10) valor_temp += 48;	//	Se o valor for menor que 10, guarda-se o caractere do numero equivalente
-			else valor_temp += 55;					//	Senao, guarda-se o caractere da letra equivalente
-			valorConvertido[posicao] = valor_temp;
-			posicao++;
-			parte_inteira /= base;
-		}
-		
-		for(i = 0; i < posicao/2; i++)				//	Swap dos valores da parte inteira, do contrario
-		{											//	os valores da parte inteira apareceriam invertidos
-			aux = valorConvertido[i+1];
-			valorConvertido[i+1] = valorConvertido[posicao-i-1];
-			valorConvertido[posicao-i-1] = aux;
-		}
-		
-		if(parte_fracionaria != 0){						// Se existir parte fracionaria, separa por um ponto
-			valorConvertido[posicao] = '.';
-			posicao++;
-		}
-		
-		while(parte_fracionaria != 0)					//	Convertendo a parte fracionaria, multiplicamos pela base
-		{												//	e armazenamos o valor da parte inteira
-			valor_temp = parte_fracionaria * base;
-			if(valor_temp < 10) valor_temp += 48;
-			else valor_temp += 55;
-			valorConvertido[posicao] = valor_temp;
-			posicao++;
-			parte_fracionaria *= base;
-			parte_fracionaria -= (int) parte_fracionaria;	//	Garante que a parte fracionaria nao possua um valor inteiro
-		}
-		
-		valorConvertido[posicao] = '\0';
-		
-	}	// Fim converteDecimal
-	
 	printf("\n\tDigite um numero decimal: ");
 	scanf("%lf%*c", &decimal);
 	
-	converteDecimal(2, binario);
-	converteDecimal(8, octal);
-	converteDecimal(16, hexadecimal);
+	converteDecimal(2, binario, decimal);
+	converteDecimal(8, octal, decimal);
+	converteDecimal(16, hexadecimal, decimal);
 	
 	printf("\n\tBinario: %s\n", binario);
 	printf("\tOctal: %s\n", octal);
@@ -171,73 +115,6 @@ void recebeSistemaLinear()
 	int numeroVariaveis, i, j;
 	double **m, *solucoes, *somatorioLinhas, *somatorioColunas;
 	int satisfazLinhas = 1, satisfazColunas = 1;
-	
-	double** criaMatriz (int l, int c)
-	{	
-		/* 	
-			Se houver memoria disponivel, cria uma matriz de double
-			com l linha e c colunas e devolve um ponteiro para a matriz;
-			Caso contrario, devolve um ponteiro nulo.
-		*/
-	
-		int i, j;
-		double** m;
-		
-		m = malloc(sizeof (double*) * l);
-		
-		if (m == NULL)	return NULL;	// Falta de memoria.
-			
-		for (i = 0; i < l; i++)
-		{
-			m[i] = malloc(sizeof(double) * c);
-			if (m[i] == NULL)		// Falta de memoria.
-			{
-				for (j = 0; j < i; j++) free (m[j]);
-				free (m);
-				return NULL;
-			}
-		}
-	
-		return m;
-		
-	}	// Fim criaMatriz
-	
-	void metodoGaussSeidel()
-	{
-		/*
-			A partir da solucao trivial, todas as variaveis = 0, calcula solucoes aproximadas
-			utilizando o Metodo de Gauss-Seidel e armazena no vetor de solucoes.
-			O algoritmo para de calcular quando a variacao nas aproximacoes seja menor que 10^-8
-			ou quando alcancar 1000 iteracoes.
-			Quando encerrada, a funcao imprime o numero de iteracoes que realizou.
-		*/
-		
-		int i, j, contaIteracoes;
-		double auxiliar;
-		
-		for(i = 0; i < numeroVariaveis; i++) solucoes[i] = 0;	// Inicializa com solucao trivial
-		
-		for(contaIteracoes = 0; contaIteracoes < 1000; contaIteracoes++)
-		{
-			for(i = 0; i < numeroVariaveis; i++)
-			{
-				auxiliar = m[i][numeroVariaveis];					// Termo independente da linha i
-				for(j = 0; j < numeroVariaveis; j++)
-				{
-					if(i != j) auxiliar += - m[i][j]*solucoes[j];
-				}
-				if(fabs(solucoes[i] - auxiliar/m[i][i]) < EPSILON)		//	Se a variacao da aproximacao foi menor que 10^-8, encerra o metodo
-				{
-					printf("\n\n\tIteracoes: %d\n", contaIteracoes);
-					return;
-				}
-				solucoes[i] = auxiliar/m[i][i];
-			}
-		}
-		
-		printf("\n\n\tIteracoes: %d", contaIteracoes);
-				
-	}	// Fim metodoGaussSeidel
 	
 	printf("\n\tDigite o nome do arquivo a ser lido: ");
 	scanf("%s%*c", nomeArquivo);
@@ -302,7 +179,7 @@ void recebeSistemaLinear()
 	{
 		if(satisfazLinhas) printf("\n\tO sistema linear satisfaz o criterio das linhas.");
 		if(satisfazColunas) printf("\n\tO sistema linear satisfaz o criterio das colunas.");
-		metodoGaussSeidel();
+		metodoGaussSeidel(numeroVariaveis, solucoes, m);
 		for(i = 0; i< numeroVariaveis; i++) printf("\n\tx[%d] = %lf", i+1, solucoes[i]);
 	}
 	
@@ -328,102 +205,6 @@ void lerEquacaoAlgebrica()
 	
 	int grauEquacao, i, contLimites = 0;
 	double *coeficientes, limites[4];
-	
-	void teoremaLagrange()
-	{
-		/*
-			Aplica o Teorema de Lagrange utlizando sua equacao caracteristica
-			e guarda o resultado no vetor de limites para ser exibido ao usuario.
-		*/
-		
-		double n, k = 0, an, b = 0;		// n: grau do polinomio, k: maior indice dos coeficientes negativos do polinomio
-										// an: coeficiente de indice n, b: modulo do menor coeficiente negativo
-		int i;
-
-		n = grauEquacao;
-		an = coeficientes[0];
-		
-		if(an < 0)											// Caso an seja negativo, k = n e teriamos uma raiz de indice 0
-		{													// se multiplicarmos o polinomio por -1 evitamos esse problema
-			for(i = 0; i<= n; i++) coeficientes[i] *= -1;
-			an *= -1;
-		}
-		
-		for(i = 0; i <= n; i++)
-		{
-			if(coeficientes[i] < 0 && k == 0) k = n-i;
-			if(coeficientes[i] < 0 && coeficientes[i] < b) b = coeficientes[i];
-		}
-		
-		b = fabs(b);	// guardamos o modulo de b
-		limites[contLimites] = 1 + pow(b/an, 1.0/(n-k));
-		
-	}	// Fim teoremaLagrange
-	
-	void metodoNewton()
-	{
-		/*
-			Utiliza o limite superior das raizes positivas da equacao como x[0]
-			e aplica o Metodo de Newton para o calculo de uma raiz aproximada.
-			
-			Ao final, imprime o resultado obtido e as iteracoes realizadas quando a variacao 
-			entre o modulo de duas aproximacoes for menor que 10^-8 ou quando ocorrerem 1000 iteracoes.
-		*/
-		
-		double aux, *derivada, x = limites[0], fx[2];	// x: aproximacao resultante
-		int contaIteracoes, i, j;						// fx: resultado do polinomio (e sua derivada) no ponto x
-		
-		derivada = malloc(sizeof(double) * grauEquacao);
-		if(derivada == NULL)
-		{
-			printf("\n\n\tFaltou memoria.\n");
-			return;
-		}
-		
-		for(i = 0; i < grauEquacao; i++)
-		{
-			if((grauEquacao-i) % 2) coeficientes[i] *= -1;		//	Trocamos o sinal dos coeficientes de indice impar para obter p(x) original
-			derivada[i] = coeficientes[i] * (grauEquacao - i);	//	Encontramos os coeficientes da derivada de f(x)
-		}
-		
-		for(i = 0; i < 1000; i++)
-		{
-			fx[0] = 0; fx[1] = 0;								//	Resetamos os valores em fx
-			for(j = 0; j <= grauEquacao; j++)
-			{
-				fx[0] += pow(x,grauEquacao-j)*coeficientes[j];						// Calcula f(x)
-				if(j != grauEquacao) fx[1] += pow(x,grauEquacao-j-1)*derivada[j];	// Calcula f'(x)
-			}
-			aux = x - fx[0]/fx[1];
-			if(fabs(aux - x) < EPSILON)
-			{
-				printf("\n\n\tIteracoes: %d\n", i);
-				printf("\n\tx = %lf", x);
-				free(derivada);
-				return;
-			}
-			x = aux;
-		}
-		
-		printf("\n\n\tIteracoes: %d\n", i);
-		printf("\n\tx = %lf\n", x);
-		free(derivada);
-		
-	}	// Fim metodoNewton
-	
-	void swap(double *a, double *b)
-	{
-		/*
-			Funcao auxiliar simples para o swap dos valores presentes 
-			em diferentes posicoes de um ponteiro double.
-		*/
-		
-		double aux;
-		
-		aux = *a;
-		*a = *b;
-		*b = aux;
-	}
 	
 	printf("\n\tDigite o grau da equacao: ");
 	scanf("%d%*c", &grauEquacao);
@@ -454,7 +235,7 @@ void lerEquacaoAlgebrica()
 		return;
 	}
 	
-	teoremaLagrange();	// Calcula L para limite superior das raizes positivas.
+	teoremaLagrange(grauEquacao, coeficientes[0], coeficientes, limites, contLimites);	// Calcula L para limite superior das raizes positivas.
 	contLimites++;
 	
 	for(i = 0; i < (grauEquacao+1)/2; i++)
@@ -462,7 +243,7 @@ void lerEquacaoAlgebrica()
 		swap(&coeficientes[i], &coeficientes[grauEquacao-i]);	// Invertemos a ordem dos coeficientes para obter X^n . p(1/x)
 	}
 	
-	teoremaLagrange();	// Calcula L1 para o Limite inferior das raizes positivas.
+	teoremaLagrange(grauEquacao, coeficientes[0], coeficientes, limites, contLimites);	// Calcula L1 para o Limite inferior das raizes positivas.
 	contLimites++;
 	
 	for(i = 0; i <= grauEquacao; i++)
@@ -470,7 +251,7 @@ void lerEquacaoAlgebrica()
 		if((grauEquacao-i) % 2) coeficientes[i] *= -1;	//	Trocamos o sinal dos coeficientes de indice impar para obter X^n . p(-1/x)
 	}
 	
-	teoremaLagrange();	// Calcula L3 para o limite superior das raizes negativas
+	teoremaLagrange(grauEquacao, coeficientes[0], coeficientes, limites, contLimites);	// Calcula L3 para o limite superior das raizes negativas
 	contLimites++;
 	
 	for(i = 0; i < (grauEquacao+1)/2; i++)				//	Invertemos a ordem dos coeficientes para obter p(-x)
@@ -478,16 +259,243 @@ void lerEquacaoAlgebrica()
 		swap(&coeficientes[i], &coeficientes[grauEquacao-i]);
 	}
 	
-	teoremaLagrange();	// Calcula L2 para o limite inferior das raizes negativas
+	teoremaLagrange(grauEquacao, coeficientes[0], coeficientes, limites, contLimites);	// Calcula L2 para o limite inferior das raizes negativas
 	
 	printf("\n\tLimites das raizes positivas: ");
 	printf("\n\t%lf <= x+ <= %lf", 1/limites[1], limites[0]);
 	printf("\n\tLimites das raizes negativas: ");
 	printf("\n\t%lf <= x- <= %lf", -limites[3], -1/limites[2]);
 	
-	metodoNewton();
+	metodoNewton(limites, grauEquacao, coeficientes);
 	
 	free(coeficientes);
 	
 }	// Fim lerEquacaoAlgebrica
 
+void converteDecimal(int base, char* valorConvertido, double decimal)
+{
+	/*
+		Converte o valor decimal dado pelo usuario.
+		A base da conversao e a string onde a conversao sera armazenada
+		sao passados como parametros.
+		
+		ex: converteDecimal(2, binario), armazena na string binario a conversao
+		do valor decimal para um sistema numerico posicional de base 2
+	*/
+	
+	int parte_inteira = (int) decimal;
+	double parte_fracionaria = decimal - parte_inteira;
+	int posicao = 1, valor_temp, i;
+	char aux;
+		
+	if(decimal < 0)						//	Verifica o sinal do decimal e garante que a conversao
+	{									//	tera o mesmo sinal
+		valorConvertido[0] = '-';
+		parte_inteira *= -1;
+	} else valorConvertido[0] = ' ';
+	
+	if(parte_inteira == 0)				// Caso nao haja parte inteira, a conversao tambem nao tera
+	{
+		valorConvertido[posicao] = '0';
+		posicao++;
+	}
+	
+	if(parte_fracionaria < 0) parte_fracionaria *= -1;
+	
+	while(parte_inteira != 0)					//	Convertendo a parte inteira, realiza-se divisoes inteiras
+	{											//	e armazenamos o valor do resto
+		valor_temp = parte_inteira % base;
+		if(valor_temp < 10) valor_temp += 48;	//	Se o valor for menor que 10, guarda-se o caractere do numero equivalente
+		else valor_temp += 55;					//	Senao, guarda-se o caractere da letra equivalente
+		valorConvertido[posicao] = valor_temp;
+		posicao++;
+		parte_inteira /= base;
+	}
+	
+	for(i = 0; i < posicao/2; i++)				//	Swap dos valores da parte inteira, do contrario
+	{											//	os valores da parte inteira apareceriam invertidos
+		aux = valorConvertido[i+1];
+		valorConvertido[i+1] = valorConvertido[posicao-i-1];
+		valorConvertido[posicao-i-1] = aux;
+	}
+	
+	if(parte_fracionaria != 0){						// Se existir parte fracionaria, separa por um ponto
+		valorConvertido[posicao] = '.';
+		posicao++;
+	}
+	
+	while(parte_fracionaria != 0)					//	Convertendo a parte fracionaria, multiplicamos pela base
+	{												//	e armazenamos o valor da parte inteira
+		valor_temp = parte_fracionaria * base;
+		if(valor_temp < 10) valor_temp += 48;
+		else valor_temp += 55;
+		valorConvertido[posicao] = valor_temp;
+		posicao++;
+		parte_fracionaria *= base;
+		parte_fracionaria -= (int) parte_fracionaria;	//	Garante que a parte fracionaria nao possua um valor inteiro
+	}
+
+	valorConvertido[posicao] = '\0';
+
+}	// Fim converteDecimal
+
+double** criaMatriz (int l, int c)
+{	
+	/* 	
+		Se houver memoria disponivel, cria uma matriz de double
+		com l linha e c colunas e devolve um ponteiro para a matriz;
+		Caso contrario, devolve um ponteiro nulo.
+	*/
+
+	int i, j;
+	double** m;
+	
+	m = malloc(sizeof (double*) * l);
+	
+	if (m == NULL)	return NULL;	// Falta de memoria.
+		
+	for (i = 0; i < l; i++)
+	{
+		m[i] = malloc(sizeof(double) * c);
+		if (m[i] == NULL)		// Falta de memoria.
+		{
+			for (j = 0; j < i; j++) free (m[j]);
+			free (m);
+			return NULL;
+		}
+	}
+
+	return m;
+	
+}	// Fim criaMatriz
+	
+void metodoGaussSeidel(int numeroVariaveis, double* solucoes, double** m)
+{
+	/*
+		A partir da solucao trivial, todas as variaveis = 0, calcula solucoes aproximadas
+		utilizando o Metodo de Gauss-Seidel e armazena no vetor de solucoes.
+		O algoritmo para de calcular quando a variacao nas aproximacoes seja menor que 10^-8
+		ou quando alcancar 1000 iteracoes.
+		Quando encerrada, a funcao imprime o numero de iteracoes que realizou.
+	*/
+	
+	int i, j, contaIteracoes;
+	double auxiliar;
+	
+	for(i = 0; i < numeroVariaveis; i++) solucoes[i] = 0;	// Inicializa com solucao trivial
+		
+	for(contaIteracoes = 0; contaIteracoes < 1000; contaIteracoes++)
+	{
+		for(i = 0; i < numeroVariaveis; i++)
+		{
+			auxiliar = m[i][numeroVariaveis];					// Termo independente da linha i
+			for(j = 0; j < numeroVariaveis; j++)
+			{
+				if(i != j) auxiliar += - m[i][j]*solucoes[j];
+			}
+			if(fabs(solucoes[i] - auxiliar/m[i][i]) < EPSILON)		//	Se a variacao da aproximacao foi menor que 10^-8, encerra o metodo
+			{
+				printf("\n\n\tIteracoes: %d\n", contaIteracoes);
+				return;
+			}
+			solucoes[i] = auxiliar/m[i][i];
+		}
+	}
+		
+	printf("\n\n\tIteracoes: %d", contaIteracoes);
+				
+}	// Fim metodoGaussSeidel
+
+void teoremaLagrange(double n, double an, double* coeficientes, double* limites, int contLimites)
+{
+	/*
+		Aplica o Teorema de Lagrange utlizando sua equacao caracteristica
+		e guarda o resultado no vetor de limites para ser exibido ao usuario.
+	*/
+	
+	double k = 0, b = 0;		// n: grau do polinomio, k: maior indice dos coeficientes negativos do polinomio
+									// an: coeficiente de indice n, b: modulo do menor coeficiente negativo
+	int i;
+		
+	if(an < 0)											// Caso an seja negativo, k = n e teriamos uma raiz de indice 0
+	{													// se multiplicarmos o polinomio por -1 evitamos esse problema
+		for(i = 0; i<= n; i++) coeficientes[i] *= -1;
+		an *= -1;
+	}
+	
+	for(i = 0; i <= n; i++)
+	{
+		if(coeficientes[i] < 0 && k == 0) k = n-i;
+			if(coeficientes[i] < 0 && coeficientes[i] < b) b = coeficientes[i];
+	}
+	
+	b = fabs(b);	// guardamos o modulo de b
+	limites[contLimites] = 1 + pow(b/an, 1.0/(n-k));
+	
+}	// Fim teoremaLagrange
+	
+void metodoNewton(double* limites, int grauEquacao, double* coeficientes)
+{
+	/*
+		Utiliza o limite superior das raizes positivas da equacao como x[0]
+		e aplica o Metodo de Newton para o calculo de uma raiz aproximada.
+		
+		Ao final, imprime o resultado obtido e as iteracoes realizadas quando a variacao 
+		entre o modulo de duas aproximacoes for menor que 10^-8 ou quando ocorrerem 1000 iteracoes.
+	*/
+		
+	double aux, *derivada, x = limites[0], fx[2];	// x: aproximacao resultante
+	int contaIteracoes, i, j;						// fx: resultado do polinomio (e sua derivada) no ponto x
+	
+	derivada = malloc(sizeof(double) * grauEquacao);
+	if(derivada == NULL)
+	{
+		printf("\n\n\tFaltou memoria.\n");
+		return;
+	}
+	
+	for(i = 0; i < grauEquacao; i++)
+	{
+		if((grauEquacao-i) % 2) coeficientes[i] *= -1;		//	Trocamos o sinal dos coeficientes de indice impar para obter p(x) original
+		derivada[i] = coeficientes[i] * (grauEquacao - i);	//	Encontramos os coeficientes da derivada de f(x)
+	}
+		
+	for(i = 0; i < 1000; i++)
+	{
+		fx[0] = 0; fx[1] = 0;								//	Resetamos os valores em fx
+		for(j = 0; j <= grauEquacao; j++)
+		{
+			fx[0] += pow(x,grauEquacao-j)*coeficientes[j];						// Calcula f(x)
+			if(j != grauEquacao) fx[1] += pow(x,grauEquacao-j-1)*derivada[j];	// Calcula f'(x)
+		}
+		aux = x - fx[0]/fx[1];
+		if(fabs(aux - x) < EPSILON)
+		{
+			printf("\n\n\tIteracoes: %d\n", i);
+			printf("\n\tx = %lf", x);
+			free(derivada);
+			return;
+		}
+		x = aux;
+	}
+	
+	printf("\n\n\tIteracoes: %d\n", i);
+	printf("\n\tx = %lf\n", x);
+	free(derivada);
+		
+}	// Fim metodoNewton
+	
+void swap(double *a, double *b)	
+{
+	/*
+		Funcao auxiliar simples para o swap dos valores presentes 
+		em diferentes posicoes de um ponteiro double.
+	*/
+		
+	double aux;
+	
+	aux = *a;
+	*a = *b;
+	*b = aux;
+
+}	// Fim swap
